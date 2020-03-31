25941901350eb5cbe3bb75533671276b82e28a9a
Faruk <faaruk@yahoo.com>
2020-03-31 23:05:48 +0600

    pdf mergring and indexing

---
+++ CBSSubmittal/Project/CreateSubmittals.aspx
@@ -0,0 +1,94 @@
+﻿<%@ Page Title="Create Submittals" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="CreateSubmittals.aspx.cs" Inherits="CBSSubmittal.Project.CreateSubmittals" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0 text-dark">Create Submittals</h1>
+                </div>
+                <!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="~/" runat="server">Home</a></li>
+                        <li class="breadcrumb-item active">Create Submittals</li>
+                    </ol>
+                </div>
+                <!-- /.col -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Create Submittals</h3>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-lg-2 col-md-2 col-sm-2">
+                            <asp:DropDownList ID="txtProjectId" runat="server" DataSourceID="SqlDataSourceUser" data-placeholder="Select Project"
+                                DataTextField="ProjectName" DataValueField="Id" CssClass="form-control">
+                            </asp:DropDownList>
+                            <asp:SqlDataSource ID="SqlDataSourceUser" runat="server" ConnectionString="<%$ ConnectionStrings:dbContext %>"
+                                SelectCommand="SELECT Id, ProjectName FROM [dbo].[Project] ORDER BY ProjectName ASC"></asp:SqlDataSource>
+
+                        </div>
+                        <div class="col-lg-2 col-md-2 col-sm-2">
+                            <asp:TextBox ID="txtDocumentName" CssClass="form-control" placeholder="Document Name" runat="server"></asp:TextBox>
+                        </div>
+                        <div class="col-lg-2 col-md-2 col-sm-2">
+                            <div class="form-group">
+                                <div class="custom-file">
+                                    <asp:FileUpload ID="fileDocumentFile" CssClass="custom-file-input" runat="server" />
+                                    <label class="custom-file-label" for="customFile">Choose file</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-4 col-sm-4">
+                            <asp:TextBox ID="txtDetails" CssClass="form-control" placeholder="Details" runat="server"></asp:TextBox>
+                        </div>
+                        <div class="col-lg-2 col-md-2 col-sm-2">
+                            <asp:Button ID="btnSaveDoocument" OnClick="btnCreate_Click" CssClass="btn btn-md btn-primary" runat="server" Text="Create" />
+                        </div>
+                        <div class="col-lg-2 col-md-2 col-sm-2">
+                            <asp:Button ID="btnCreateSubmittals" OnClick="btnCreateSubmittals_Click" CssClass="btn btn-md btn-primary" runat="server" Text="Submittals" />
+                        </div>
+                    </div>
+                </div>
+                <!-- /.card-body -->
+            </div>
+
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Documents</h3>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body">
+                    <asp:GridView ID="grdDocument" AutoGenerateColumns="false" runat="server" CssClass="table table-striped table-bordered" OnRowDeleting="GridView_RowDeleting" DataKeyNames="Id">
+                        <Columns>
+                            <asp:BoundField DataField="ProjectName" HeaderText="Project Name" />
+                            <asp:BoundField DataField="DocumentName" HeaderText="Document Name" />
+                            <asp:BoundField DataField="Details" HeaderText="Details" />
+                            <asp:TemplateField HeaderStyle-Width="20">
+                                <ItemTemplate>
+                                    <a id="downloadLink" class="btn btn-info btn-xs" title="Downlaod" href="CreateDocument.aspx?Id=<%#Eval("Id") %>">
+                                        <i class="fas fa-download"></i>
+                                    </a>
+                                    <asp:Button ID="deletebtn" runat="server" CommandName="Delete" CssClass="btn btn-danger btn-xs"
+                                        Text="&nbsp;&nbsp;X&nbsp;&nbsp;" OnClientClick="return confirm('Are you sure you want to delete this item?');" />
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                        </Columns>
+                    </asp:GridView>
+                </div>
+                <!-- /.card-body -->
+            </div>
+        </div>
+    </section>
+</asp:Content>
---
+++ CBSSubmittal/Project/CreateSubmittals.aspx.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Web;
+using System.Web.UI.WebControls;
+using PdfSharp;
+using PdfSharp.Drawing;
+using PdfSharp.Drawing.Layout;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.Advanced;
+using PdfSharp.Pdf.IO;
+
+namespace CBSSubmittal.Project
+{
+    public partial class CreateSubmittals : System.Web.UI.Page
+    {
+        SqlConnection dbConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["dbContext"].ConnectionString);
+        string path = HttpContext.Current.Request.PhysicalApplicationPath;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                this.BindGrid();
+            }
+
+            string docId = Request.QueryString["Id"];
+            if (docId != null)
+            {
+                int DID = Convert.ToInt32(docId);
+                string query = "SELECT DocumentFile FROM [dbo].[Document] WHERE Id=" + DID;
+                SqlCommand cmd = new SqlCommand(query, dbConnection);
+                string fileName = cmd.ExecuteScalar().ToString();
+
+                Response.ContentType = "Application/pdf";
+                Response.AppendHeader("Content-Disposition", "attachment; filename=~/Uploads/Documents/" + fileName);
+                Response.TransmitFile(Server.MapPath(@"~/Uploads/Documents/" + fileName));
+                Response.End();
+                dbConnection.Close();
+            }
+
+        }
+
+        private void BindGrid()
+        {
+            dbConnection.Open();
+            using (SqlCommand cmd = new SqlCommand("SELECT D.Id, P.ProjectName, D.DocumentName, D.DocumentFile, D.Details FROM [dbo].[Document] D LEFT JOIN [dbo].[Project] P ON D.ProjectId=P.Id", dbConnection))
+            {
+                using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                {
+                    DataTable dt = new DataTable();
+                    sda.Fill(dt);
+                    grdDocument.DataSource = dt;
+                    grdDocument.DataBind();
+
+                    if (grdDocument.Rows.Count > 0)
+                    {
+                        //Adds THEAD and TBODY Section.
+                        grdDocument.HeaderRow.TableSection = TableRowSection.TableHeader;
+
+                        //Adds TH element in Header Row.  
+                        grdDocument.UseAccessibleHeader = true;
+
+                        //Adds TFOOT section. 
+                        grdDocument.FooterRow.TableSection = TableRowSection.TableFooter;
+                    }
+                }
+            }
+        }
+
+        protected void btnCreate_Click(object sender, EventArgs e)
+        {
+            string strSaveFileAs = "";
+
+            strSaveFileAs = Server.MapPath("~/Uploads/Documents/" + fileDocumentFile.FileName.Replace(" ", "_").ToLower());
+
+            try
+            {
+                if (fileDocumentFile.HasFile)
+                {
+                    fileDocumentFile.SaveAs(strSaveFileAs);
+
+                    dbConnection.Open();
+                    string query = "INSERT INTO [dbo].[Document] ([ProjectId],[DocumentName],[DocumentFile],[Details]) VALUES (@ProjectId,@DocumentName,@DocumentFile,@Details)";
+
+                    SqlCommand cmd = new SqlCommand(query, dbConnection);
+                    cmd.Parameters.AddWithValue("@ProjectId", Convert.ToInt32(txtProjectId.Text).ToString());
+                    cmd.Parameters.AddWithValue("@DocumentName", txtDocumentName.Text);
+                    cmd.Parameters.AddWithValue("@DocumentFile", fileDocumentFile.FileName.Replace(" ", "_").ToLower());
+                    cmd.Parameters.AddWithValue("@Details", txtDetails.Text);
+                    cmd.ExecuteNonQuery();
+                    Response.Redirect("~/Project/CreateDocument.aspx");
+                }
+                else
+                {
+                    Response.Write("No file was uploaded.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("error" + ex.ToString());
+            }
+            finally
+            {
+                dbConnection.Close();
+            }
+        }
+        protected void btnCreateSubmittals_Click(object sender, EventArgs e)
+        {
+            string projectName = "Project 1";
+            // Create the output document
+            PdfDocument outputDocument = new PdfDocument();
+
+            // Show consecutive pages facing. Requires Acrobat 5 or higher.
+            outputDocument.PageLayout = PdfPageLayout.SinglePage;
+            
+            XFont font = new XFont("Verdana", 10, XFontStyle.Bold);
+            XStringFormat format = new XStringFormat();
+            format.Alignment = XStringAlignment.Center;
+            format.LineAlignment = XLineAlignment.Far;
+            XGraphics gfx;
+            XRect box;
+
+            // Create an empty page
+            PdfPage page = outputDocument.AddPage();
+            // Create a font
+            font = new XFont("Verdana", 15, XFontStyle.BoldItalic);
+            // Get an XGraphics object for drawing
+            gfx = XGraphics.FromPdfPage(page);
+
+            int positionY = 60;
+            int indexCount = 2;
+            int pageNo = 2;
+
+            dbConnection.Open();
+            using (SqlCommand cmd = new SqlCommand("SELECT D.Id, P.ProjectName, D.DocumentName, D.DocumentFile, D.Details FROM [dbo].[Document] D LEFT JOIN [dbo].[Project] P ON D.ProjectId=P.Id WHERE D.Id<>6", dbConnection))
+            {
+                using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                {
+                    DataTable dt = new DataTable();
+                    sda.Fill(dt);
+                    if (dt.Rows.Count > 0)
+                    {
+                        outputDocument.Info.Title = dt.Rows[0]["ProjectName"].ToString();;
+
+                        foreach (DataRow row in dt.Rows)
+                        {
+                            string filex = row["DocumentName"].ToString() + "        " + indexCount;
+                            
+                            string filenamex = path + "/Uploads/Documents/" + row["DocumentFile"].ToString();
+                            PdfDocument inputDocumentx = PdfReader.Open(filenamex, PdfDocumentOpenMode.Import);
+                            int count = inputDocumentx.PageCount;
+
+                            //// Draw the text
+                            //gfx.DrawString(filex, font, XBrushes.Black,
+                            //  new XRect(0, positionY, page.Width, page.Height),
+                            //  XStringFormats.Center);
+                            
+                            XFont fontNormal = new XFont("Verdana", 15, XFontStyle.Regular);
+                            
+                            //Set link position
+                            var xrect = new XRect(0, positionY, page.Width, 20);
+                            var rect = gfx.Transformer.WorldToDefaultPage(xrect);
+                            var pdfrect = new PdfRectangle(rect);
+
+                            //file link
+                            //page.AddFileLink(pdfrect, filename3);
+                            //gfx2.DrawString("open file CompareDocument1_tempfile.pdf", fontNormal, XBrushes.Black, xrect, XStringFormats.TopLeft);
+
+                            //Goto  Page 2
+                            page.AddDocumentLink(pdfrect, indexCount);
+                            gfx.DrawString(filex, fontNormal, XBrushes.Black, xrect, XStringFormats.Center);
+
+                            //TheArtOfDev.HtmlRenderer.PdfSharp.HtmlContainer c = new TheArtOfDev.HtmlRenderer.PdfSharp.HtmlContainer();
+                            //c.SetHtml("<html><body style='font-size:20px'>Whatever</body></html>");
+                            //c.PerformPaint(gfx);
+
+                            positionY += 20;
+                            indexCount += count;
+                        }
+
+                        font = new XFont("Verdana", 10, XFontStyle.Bold);
+                        foreach (DataRow row in dt.Rows)
+                        {
+                            string filex = row["DocumentFile"].ToString();
+                            string filenamex = path + "/Uploads/Documents/" + filex;
+                            PdfDocument inputDocumentx = PdfReader.Open(filenamex, PdfDocumentOpenMode.Import);
+                            int count2 = inputDocumentx.PageCount;
+                            for (int idx = 0; idx < count2; idx++)
+                            {
+                                // Get page from 1st document
+                                PdfPage page1 = inputDocumentx.PageCount > idx ?
+                                  inputDocumentx.Pages[idx] : new PdfPage();
+
+                                // Add pages to the output document
+                                page1 = outputDocument.AddPage(page1);
+
+                                // Write document file name and page number on each page
+                                gfx = XGraphics.FromPdfPage(page1);
+                                box = page1.MediaBox.ToXRect();
+                                box.Inflate(0, -10);
+                                gfx.DrawString(String.Format("{0}  {1}", "Page - ", pageNo),
+                                  font, XBrushes.Red, box, format);
+                                pageNo += 1;
+                            }
+                        }
+
+                        // Save the document...
+                        string filename3 = path+"/Uploads/Documents/" + projectName+ ".pdf";
+                        outputDocument.Save(filename3);
+                        Process.Start(filename3);
+                    }
+                }
+            }
+        }
+
+        protected void GridView_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            try
+            {
+                dbConnection.Open();
+                int DID = Convert.ToInt32(grdDocument.DataKeys[e.RowIndex].Value);
+                string query = "DELETE FROM [dbo].[Document] WHERE Id=" + DID;
+                SqlCommand cmd = new SqlCommand(query, dbConnection);
+                cmd.ExecuteNonQuery();
+                Response.Redirect("~/Project/CreateDocument.aspx");
+            }
+            catch (Exception ex)
+            {
+                Response.Write("error" + ex.ToString());
+            }
+            finally
+            {
+                dbConnection.Close();
+
+            }
+        }
+    }
+}
